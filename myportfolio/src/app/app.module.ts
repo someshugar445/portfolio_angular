126349f730935b29ff35f8a47726f3c1522a9dc2
someshugar445 <someshugar@gmail.com>
2021-07-24 19:07:18 +0530

    AUT-9 update code

--- myportfolio/src/app/app.module.ts
+++ myportfolio/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './components/about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- myportfolio/src/app/components/home/home.component.ts
+++ myportfolio/src/app/components/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('myanimation',[
+       state('smaller',style({
+          transform : 'translateY(100px)'
+       })),
+       state('larger',style({
+          transform : 'translateY(0px)'
+       })),
+       transition('smaller <=> larger',animate('300ms ease-in'))
+    ])
+ ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  state1: string = "smaller";
+  state2: string = "smaller";
+  state3: string = "smaller";
+   animate() {
+      this.state1= this.state1 == 'larger' ? 'smaller' : 'larger';
+   }
+   animate2() {
+    this.state2= this.state2 == 'larger' ? 'smaller' : 'larger';
+ }
+ animate3() {
+  this.state3= this.state3 == 'larger' ? 'smaller' : 'larger';
+}
 }
